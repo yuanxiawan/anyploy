fcf2f1e648e16843375a2f8fec11ffa25af701a1
yuanxiawan <33147965+yuanxiawan@users.noreply.github.com>
2025-04-18 19:53:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import akshare as ak
+import time
 
 try:
-    stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
+    stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em(timeout=30)
     print(stock_zh_a_spot_em_df)
+    time.sleep(5)  # 等待 5 秒
 except Exception as e:
     print(f"获取数据失败: {e}")
     print("请检查网络连接、AkShare 版本以及 AkShare 服务器状态。")
+
